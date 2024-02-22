68997398a528ec2b85c969cf50167ed2a6a2e161
Frédéric Godard <godard.fred@gmail.com>
2024-02-22 18:37:17 +0100

    typage des données core

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,31 +1,51 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import {BehaviorSubject, map, Subject, take} from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import {OlympicCountry} from "../models/Olympic";
 
 @Injectable({
   providedIn: 'root',
 })
+
+/**
+ * Service fournissant les données des précédents résultats Olympiques par Pays.
+ */
 export class OlympicService {
+
+  //url du service fournissant les données (ici mocké dans fichier json resource)
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
 
-  constructor(private http: HttpClient) {}
+  //Subject permettant la mise en cache du résultat d'appel du service.
+  private olympics$: Subject<OlympicCountry[]> = new BehaviorSubject<OlympicCountry[]>([]);
 
-  loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
-      tap((value) => this.olympics$.next(value)),
+  /**
+   * Constructor
+   * @param http client http
+   */
+  public constructor(private http: HttpClient) {}
+
+  /**
+   * Appel et met en cache le résultat du service dans le BehaviorSubject
+   */
+  public loadInitialData() {
+    return this.http.get<OlympicCountry[]>(this.olympicUrl).pipe (
+      tap((value : OlympicCountry[]) => this.olympics$.next(value)),
       catchError((error, caught) => {
-        // TODO: improve error handling
         console.error(error);
+        // tableau vide car pas de données disponibles
+        this.olympics$.next([]);
         // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
         return caught;
       })
     );
   }
 
-  getOlympics() {
+  /**
+   * Retourne l'observable permettant de délivrer le résultat de service.
+   */
+  public getOlympics() {
     return this.olympics$.asObservable();
   }
+
 }
